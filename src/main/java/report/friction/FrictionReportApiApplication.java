5726c1616a83357acb7230fd4c8e1d6b57a1b49b
PierceBeckett <pierceabeckett@gmail.com>
2023-05-10 19:40:06 -0500

    Initial Commit

---
+++ src/main/java/report/friction/FrictionReportApiApplication.java
@@ -0,0 +1,13 @@
+package report.friction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrictionReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrictionReportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/report/friction/FrictionReportApiApplicationTests.java
@@ -0,0 +1,14 @@
+package report.friction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrictionReportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//to be implemented
+	}
+
+}
