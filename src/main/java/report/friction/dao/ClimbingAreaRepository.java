86a80a87c5ab35b67a6d6184c986ca7cd094e21e
PierceBeckett <pierceabeckett@gmail.com>
2023-05-12 14:34:08 -0500

    hooked up the area service and repo

--- src/main/java/report/friction/dao/ClimbingAreaRepository.java
+++ src/main/java/report/friction/dao/ClimbingAreaRepository.java
@@ -1,4 +1,10 @@
 package report.friction.dao;
 
-public class ClimbingAreaRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClimbingAreaRepository extends CrudRepository<ClimbingAreaEntity, String> {
+
+    ClimbingAreaEntity findByAreaName(String areaName);
 }
--- src/main/java/report/friction/services/ClimbingAreaServiceImpl.java
+++ src/main/java/report/friction/services/ClimbingAreaServiceImpl.java
@@ -1,13 +1,29 @@
 package report.friction.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import report.friction.dao.ClimbingAreaRepository;
 import report.friction.models.ClimbingAreaObj;
 
 @Service
 public class ClimbingAreaServiceImpl implements ClimbingAreaService{
 
+    @Value("${OPEN_WEATHER_API_KEY}")
+    private String openWeatherApiKey;
+
+    private static final String OPEN_WEATHER_DOMAIN = "https://api.openweathermap.org/data/3.0/onecall?";
+
+    private final ClimbingAreaRepository climbingAreaRepository;
+
+    @Autowired
+    public ClimbingAreaServiceImpl(ClimbingAreaRepository climbingAreaRepository){
+        this.climbingAreaRepository = climbingAreaRepository;
+    }
+
     @Override
     public ClimbingAreaObj getClimbingAreaData(String areaName) {
+        System.out.println(openWeatherApiKey);
         return null;
     }
 }
